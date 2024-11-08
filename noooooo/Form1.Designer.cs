74a091fb3c0f3453e6afdc2c2ef4bb61255597e0
Lenovo <Lenovo@DESKTOP-10R3SAU>
2024-11-07 22:41:36 -0600

    Agregar archivos de proyecto.

---
+++ noooooo/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace noooooo
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Definición de los controles
+        private System.Windows.Forms.TextBox TBUsuario;
+        private System.Windows.Forms.TextBox TBTempC;
+        private System.Windows.Forms.TextBox TBTempF;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblTemperaturaC;
+        private System.Windows.Forms.Label lblTemperaturaF;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox TBDate;
+        private System.Windows.Forms.Label lblLEDStatus;
+        private System.Windows.Forms.CheckBox checkBoxLED;
+
+        // Función de disposición de los componentes
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Método de inicialización del formulario y los controles
+        private void InitializeComponent()
+        {
+            this.TBUsuario = new System.Windows.Forms.TextBox();
+            this.TBTempC = new System.Windows.Forms.TextBox();
+            this.TBTempF = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lblTemperaturaC = new System.Windows.Forms.Label();
+            this.lblTemperaturaF = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.TBDate = new System.Windows.Forms.TextBox();
+            this.lblLEDStatus = new System.Windows.Forms.Label();
+            this.checkBoxLED = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TBUsuario
+            // 
+            this.TBUsuario.Location = new System.Drawing.Point(81, 121);
+            this.TBUsuario.Name = "TBUsuario";
+            this.TBUsuario.Size = new System.Drawing.Size(100, 20);
+            this.TBUsuario.TabIndex = 0;
+            this.TBUsuario.TextChanged += new System.EventHandler(this.TBUsuario_TextChanged);
+            // 
+            // TBTempC
+            // 
+            this.TBTempC.Location = new System.Drawing.Point(365, 122);
+            this.TBTempC.Name = "TBTempC";
+            this.TBTempC.Size = new System.Drawing.Size(100, 20);
+            this.TBTempC.TabIndex = 1;
+            // 
+            // TBTempF
+            // 
+            this.TBTempF.Location = new System.Drawing.Point(365, 159);
+            this.TBTempF.Name = "TBTempF";
+            this.TBTempF.Size = new System.Drawing.Size(100, 20);
+            this.TBTempF.TabIndex = 2;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(162, 241);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(229, 68);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblTemperaturaC
+            // 
+            this.lblTemperaturaC.AutoSize = true;
+            this.lblTemperaturaC.Location = new System.Drawing.Point(267, 122);
+            this.lblTemperaturaC.Name = "lblTemperaturaC";
+            this.lblTemperaturaC.Size = new System.Drawing.Size(84, 13);
+            this.lblTemperaturaC.TabIndex = 4;
+            this.lblTemperaturaC.Text = "Temperatura C°:";
+            // 
+            // lblTemperaturaF
+            // 
+            this.lblTemperaturaF.AutoSize = true;
+            this.lblTemperaturaF.Location = new System.Drawing.Point(267, 162);
+            this.lblTemperaturaF.Name = "lblTemperaturaF";
+            this.lblTemperaturaF.Size = new System.Drawing.Size(83, 13);
+            this.lblTemperaturaF.TabIndex = 5;
+            this.lblTemperaturaF.Text = "Temperatura F°:";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(12, 121);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 6;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // TBDate
+            // 
+            this.TBDate.Location = new System.Drawing.Point(81, 152);
+            this.TBDate.Name = "TBDate";
+            this.TBDate.Size = new System.Drawing.Size(100, 20);
+            this.TBDate.TabIndex = 7;
+            // 
+            // lblLEDStatus
+            // 
+            this.lblLEDStatus.AutoSize = true;
+            this.lblLEDStatus.Location = new System.Drawing.Point(54, 241);
+            this.lblLEDStatus.Name = "lblLEDStatus";
+            this.lblLEDStatus.Size = new System.Drawing.Size(62, 13);
+            this.lblLEDStatus.TabIndex = 8;
+            this.lblLEDStatus.Text = "Apaga LED";
+            // 
+            // checkBoxLED
+            // 
+            this.checkBoxLED.AutoSize = true;
+            this.checkBoxLED.Location = new System.Drawing.Point(81, 282);
+            this.checkBoxLED.Name = "checkBoxLED";
+            this.checkBoxLED.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxLED.TabIndex = 9;
+            this.checkBoxLED.UseVisualStyleBackColor = true;
+            this.checkBoxLED.CheckedChanged += new System.EventHandler(this.checkBoxLED_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Fecha:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(960, 514);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxLED);
+            this.Controls.Add(this.lblLEDStatus);
+            this.Controls.Add(this.TBDate);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblTemperaturaF);
+            this.Controls.Add(this.lblTemperaturaC);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.TBTempF);
+            this.Controls.Add(this.TBTempC);
+            this.Controls.Add(this.TBUsuario);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ noooooo/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+using System.IO.Ports;
+using System.Data.SqlClient;
+using System.Reflection.Emit;
+
+namespace noooooo
+{
+    public partial class Form1 : Form
+    {
+        string Sqlconnection = "Server=localhost;port=3306;Database=exapractico1;Uid=root;Pwd=laluznu7S!;";
+        MySqlConnection connection;
+        SerialPort Portino;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection = new MySqlConnection(Sqlconnection);
+            Portino = new SerialPort("COM5", 9600);
+            try
+            {
+                Portino.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Couldn't connect to Arduino: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateTemp();
+        }
+        private void UpdateTemp()
+        {
+            float tempC = 25.5f;
+            float tempF = (tempC * 9 / 5) + 32;
+
+            TBTempC.Text = tempC.ToString("0.00");
+            TBTempF.Text = tempF.ToString("0.00");
+            lblLEDStatus.Text = "LED: " + (checkBoxLED.Checked ? "On" : "Off");
+        }
+        private void ButtSave()
+        {
+            try
+            {
+                connection.Open();
+                string query = "INSERT INTO pepa (usuario, temperaturaC, temperaturaF, fecha, ledEstado) VALUES (@usuario, @temperaturaC, @temperaturaF, @fecha, @ledEstado)";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                cmd.Parameters.AddWithValue("@usuario", TBUsuario.Text);
+                cmd.Parameters.AddWithValue("@temperaturaC", TBTempC.Text);
+                cmd.Parameters.AddWithValue("@temperaturaF", TBTempF.Text);
+
+                DateTime fechaSeleccionada;
+                if (DateTime.TryParse(TBDate.Text, out fechaSeleccionada))
+                {
+                    cmd.Parameters.AddWithValue("@fecha", fechaSeleccionada.ToString("yyyy-MM-dd"));
+                }
+                else
+                {
+                    MessageBox.Show("Write a valid entry like yyyy-MM-dd.");
+                    return;
+                }
+
+                cmd.Parameters.AddWithValue("@ledEstado", checkBoxLED.Checked ? 1 : 0);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Data saved.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while saving data: " + ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private bool isValidText(string str)
+        {
+            return Regex.IsMatch(str, @"^[a-zA-Z\s]+$");
+        }
+
+        private void checkText(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text.Length != 0)
+            {
+                if (!isValidText(textBox.Text))
+                {
+                    MessageBox.Show("Ingrese un valor valido para usuario", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox.Clear();
+                }
+            }
+        }
+
+        private void LEDControl()
+        {
+            if (Portino.IsOpen)
+            {
+                if (checkBoxLED.Checked)
+                {
+                    Portino.Write("1"); // Turns On LED
+                    lblLEDStatus.Text = "LED: On";
+                    MessageBox.Show("The LED is On");
+                }
+                else
+                {
+                    Portino.Write("0"); // Turns Off LED
+                    lblLEDStatus.Text = "LED: Off";
+                    MessageBox.Show("The LED is Off");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, Something happened¿.");
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            ButtSave();
+        }
+
+        private void checkBoxLED_CheckedChanged(object sender, EventArgs e)
+        {
+            LEDControl();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (Portino.IsOpen)
+            {
+                Portino.Close();
+            }
+            base.OnFormClosing(e);
+        }
+
+        private void TBUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

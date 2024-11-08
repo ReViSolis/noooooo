28b1937d5aca3cb2df99979b2e4193f7f059eef3
ReViSolis <reyli.solis52@unach.mx>
2024-11-07 22:46:46 -0600

    Add files via upload

---
+++ Exapractico1.sql
@@ -0,0 +1,7 @@
+create database exapractico1;
+use exapractico1;
+
+create table exabla (id int not null auto_increment, usuario varchar(20) not null,
+mes int(10) not null, dia int(10) not null, año int(10) not null, primary key (id));
+
+select * from exabla;
